1b1a10d3fd8804d07fcf5b65cd6c13ac0497a2fe
RileyVaughn <rj.vaughn@live.com>
2024-11-18 00:01:49 -0700

    Modified filenames

--- src/main/java/com/truemovement/TMPlugin.java
+++ src/main/java/com/truemovement/TMPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.truemovement;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "True Movement"
 )
-public class ExamplePlugin extends Plugin
+public class TMPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private TMConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	TMConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(TMConfig.class);
 	}
 }
